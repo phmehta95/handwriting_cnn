b5bdc4080503839dfbc1ff3da7bec925b49ebb44
Pruthvi Mehta <qthedesperate@gmail.com>
2023-12-02 22:12:10 +0000

    got predictor working but not very accurate

--- number_predictor.py
+++ number_predictor.py
@@ -6,19 +6,35 @@ from matplotlib import image as mpimg
 import os
 import sys
 import time
-
+from PIL import Image 
+import cv2
 
 reconstructed_model = tf.keras.models.load_model("/home/pruthvi/Desktop/handwriting_cnn/handwriting_cnn.h5")
 
-directory = "/home/pruthvi/Desktop/handwriting_cnn/external_test_data/number1.png"
+directory = "/home/pruthvi/Desktop/handwriting_cnn/external_test_data/number6.png"
 
 #Predicting images
+image = cv2.imread(directory, cv2.IMREAD_GRAYSCALE) #convert to greyscale 
+image = cv2.resize(image, (28,28), interpolation=cv2.INTER_LINEAR)
+image = cv2.bitwise_not(image)
+np.set_printoptions(suppress=True,linewidth=np.nan)
+#image = np.asarray(image)
+#image = image/255
 
-img = image.load_img("/home/pruthvi/Desktop/handwriting_cnn/external_test_data/number1.png", target_size=(28,28))
+print(image)
+print()
+print(image.shape)
 
-plt.imshow(img)
+plt.imshow(image)
 plt.show()
+image = image.reshape(784)
 
-classes = reconstructed_model.predict(img)
-print(classes)
+print(image.shape)
+
+
+pred = reconstructed_model.predict(np.expand_dims(image,0))
+print(pred.argmax())
+
+#classes = reconstructed_model.predict(img)
+#print(classes)
 ####can't do it like this, need to convert image of number to MNIST -> return pixel value of each pixel in the image#####
